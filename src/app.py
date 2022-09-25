0c9e53331c5366a18269a4cfa9ca5e0c3ac81d9e
Jymy Korhonen <jymyko@gmail.com>
2022-09-25 22:43:52 +0300

    Reformat code

--- src/app.py
+++ src/app.py
@@ -3,42 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from os import getenv
 from dotenv import load_dotenv
+from db_interactions import create_user, get_password_hash
 
 load_dotenv()
-
-from db_interactions import create_user, get_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
-# Pages
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        password_hash = get_password_hash(db, username)
-        if check_password_hash(password, password_hash):
-            session["username"] = username
-            return redirect("/")
-        else:
-            return ""
-    return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        create_user(db, username, password)
-        return redirect("/")
-
-    return render_template("register.html")
+import routes
---
+++ src/routes.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, session
+from werkzeug.security import check_password_hash
+from app import app, db
+
+from db_interactions import create_user, get_password_hash
+
+
+# Pages
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        password_hash = get_password_hash(db, username)
+        print(password_hash, password)
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return render_template(
+                "error.html", error="Väärä salasana tai käyttäjänimi", return_to="/"
+            )
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+
+@app.route("/logout", methods={"POST"})
+def logout():
+    del session["username"]
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        create_user(db, username, password)
+        return redirect("/")
+
+    if request.method == "GET":
+        return render_template("register.html")
