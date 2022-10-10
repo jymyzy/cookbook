e6f0ab59130d11aaf2581a0407ef82a048df6e78
Jymy Korhonen <jymyko@gmail.com>
2022-10-10 03:04:28 +0300

    Fix add_recipe and add add_ingredients_to_reciepes

--- src/db_interactions.py
+++ src/db_interactions.py
@@ -22,8 +22,33 @@ def get_ingreadients(db):
     return db.session.execute(sql, {}).fetchall()
 
 
-def create_recipe(db, name: str, description: str, instructions: str):
-    sql = "INSERT INTO recipes (name, description, instructions) VALUES (:name, :description, :instructions)"
-    db.session.execute(
-        sql, {"name": name, "description": description, "instructions": instructions}
+def add_recipe(db, name: str, description: str, instructions: str, author_id: int):
+    sql = "INSERT INTO recipes (name, desprictions, instructions, author_id, created_on) VALUES (:name, :desprictions, :instructions, :author_id, NOW()) RETURNING id"
+    result = db.session.execute(
+        sql,
+        {
+            "name": name,
+            "desprictions": description,
+            "instructions": instructions,
+            "author_id": author_id,
+        },
     )
+    id = result.fetchone()[0]
+    db.session.commit()
+    return id
+
+
+def add_ingredient_to_recipe(
+    db, recipe_id: int, ingredient_id: int, amount: int, unit: str
+):
+    sql = "INSERT INTO ingredientsInRecipe (recipe_id, ingredient_id, amount, unit) VALUES (:recipe_id, :ingredient_id, :amount, :unit)"
+    db.session.execute(
+        sql,
+        {
+            "recipe_id": recipe_id,
+            "ingredient_id": ingredient_id,
+            "amount": amount,
+            "unit": unit,
+        },
+    )
+    db.session.commit()
